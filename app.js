25f2df87ae7aaeee48d6fe8ffda2f2b8603ba720
Deivid Martins <md031194@gmail.com>
2022-06-18 00:43:15 -0300

    Criado Funcao Gravar Despesas no localStorage

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
 class Depesa {
-  constructor(ano,mes,dia,tipo,descrisao,valor){
-    this.ano = ano
-    this.mes = mes
-    this.dia = dia
-    this.tipo = tipo
-    this.descricao = descrisao
-    this.valor = valor
+  constructor(ano, mes, dia, tipo, descricao, valor) {
+    this.ano = ano;
+    this.mes = mes;
+    this.dia = dia;
+    this.tipo = tipo;
+    this.descricao = descricao;
+    this.valor = valor;
   }
- }
-
-function cadastartDespesa(){
- let  ano = document.getElementById('ano')
-  let  mes = document.getElementById('mes')
-  let dia =document.getElementById('dia')
-  let tipo = document.getElementById('tipo')
-  let descricao = document.getElementById('descricao')
-   let valor = document.getElementById('valor')
-   
-   let despesa = new Depesa(ano.value, mes.value, tipo.value, descricao.value, valor.value)
-
-   console.log(despesa)
-  
 }
 
+function cadastrarDespesa() {
+  let ano = document.getElementById("ano");
+  let mes = document.getElementById("mes");
+  let dia = document.getElementById("dia");
+  let tipo = document.getElementById("tipo");
+  let descricao = document.getElementById("descricao");
+  let valor = document.getElementById("valor");
+
+  let despesa = new Depesa(
+    ano.value,
+    mes.value,
+    dia.value,
+    tipo.value,
+    descricao.value,
+    valor.value
+  );
+
+  gravar(despesa);
+}
+
+function gravar(d) {
+  localStorage.setItem("despesa", JSON.stringify(d));
+}
